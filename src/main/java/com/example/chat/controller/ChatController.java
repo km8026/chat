60428246d2124f12dd069585ee844f34c317c946
MinJunKim <167392999+km8026@users.noreply.github.com>
2024-06-21 15:10:20 +0900

    asd

---
+++ src/main/java/com/example/chat/controller/ChatController.java
@@ -0,0 +1,12 @@
+package com.example.chat.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ChatController {
+    @GetMapping("/chat")
+    public String chat() {
+        return "chat";
+    }
+}
